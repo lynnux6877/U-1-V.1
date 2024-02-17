be9e4ce4f7fd774a412211a9a8fe677bda8f2e5e
lynnux6877 <158472357+lynnux6877@users.noreply.github.com>
2024-02-17 14:19:30 -0300

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -7,6 +7,11 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+@client.event
+async def on_ready():
+    print(f"O {client.user.name} ficou ligado!")
+    await client.change_presence(activity=discord.CustomActivity(emoji="👉",name="DE LINNUX PARA O MUNDO"))
+
 histories = {}
 
 async def send_long_message(channel, message):
